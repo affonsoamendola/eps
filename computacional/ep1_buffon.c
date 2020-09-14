25dfaa2b15b86471269ed38659be60198ab75413
Affonso Amendola <affonsoamendola@bol.com.br>
2020-09-14 10:33:31 -0300

    Did a bunch of stuff

--- computacional/ep1_buffon.c
+++ computacional/ep1_buffon.c
@@ -3,6 +3,7 @@
 //By Affonso Amendola
 
 //Code licensed under GPLv3, available on my github.
+//https://github.com/affonsoamendola/eps/tree/master/computacional
 
 //So Alex, I chose to do it in C, despite the many issues with rand() and srand()
 //mainly because of the speed, I'd be really curious to see the differences between
@@ -15,38 +16,89 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
-const float strip_size_x = 4.0f;
-const int number_of_strips = 100;
+const double strip_size_x = 4.0;
+const int number_of_strips = 1;
 
-const float field_size_x = strip_size_x * number_of_strips;
-const float field_size_y = 100.0f;
+const double field_size_x = strip_size_x * number_of_strips;
+const double field_size_y = 100.0;
 
-const float needle_size = 4.0f;
+const double needle_length = 4.0;
 
-float get_random()
+double get_random()
 {
 	//There's a great talk on why rand()%value is crap, 
 	//but in this case I believe this returns a uniform distribution, if rand()
 	//is uniform.
 
 	//https://channel9.msdn.com/Events/GoingNative/2013/rand-Considered-Harmful
-	return (float)rand()/(float)RAND_MAX;
+	return (double)rand()/(double)RAND_MAX;
 }
 
 //Since this is C, you cant return 2 values like in modern languages,
 //a common solution to that is using pointer arguments to move values outside of
 //the scope.
-void get_random_position(float* x_out, float* y_out, float max_x, float max_y)
+void get_random_position(double* x_out, double* y_out, double max_x, double max_y)
 {
-	float x;
-	float y;
+	double x;
+	double y;
 
 	x = get_random() * max_x;
 	y = get_random() * max_y;
 
 	*x_out = x;
 	*y_out = y;
+}
+
+int crossing_boards(double x, double footprint)
+{
+	//This puts all of the needles on the same strip, since all of them are equal
+	//this makes the math simpler
+	while(x - strip_size_x >= 0.0) x -= strip_size_x;
+
+	double half_footprint = footprint/2.0;
+
+	if(x + half_footprint >= strip_size_x || x - half_footprint <= 0.0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void buffon_experiment(int number_of_needles)
+{
+	int crossed = 0;
+
+	for(int i = 0; i < number_of_needles; i++)
+	{
+		double needle_x;
+		double needle_y;
+		double needle_angle;
+
+		get_random_position(&needle_x, &needle_y, field_size_x, field_size_y);
+
+		//I believe it is not nescessary to simulate all angle quadrants of needles, 
+		//since they are all mirror versions of each other.
+		needle_angle = get_random() * 90.0;
+
+		double needle_footprint = cos(needle_angle * M_PI / 180.0) * needle_length;
+
+		if(crossing_boards(needle_x, needle_footprint))
+		{
+			crossed += 1;
+		}
+	}
+
+	double pi_approximation = (2.0 * needle_length * number_of_needles) / (strip_size_x * crossed);
+
+	double error = fabs(pi_approximation - M_PI);
+
+	printf("Pi is approximately = %.50f\n", pi_approximation);
+	printf("Error = %.50f\n", error);
 }
 
 int main(int argc, char const *argv[])
@@ -56,20 +108,33 @@ int main(int argc, char const *argv[])
 	//This is bad, time() returns the amount of seconds since jan 1, 1970, meaning
 	//that running this software two times in the same second gets the same result
 	//
-	//Which isn't that unlikely.
+	//Which isn't that unlikely, if for example this was running on multiple threads
+	//And they were initialized on the same tick of the CPU, they would always
+	//return the same values.
 	//
-	//But on C without depending on external libraries 
+	//But on C without depending on external libraries
 	//(Or doing some magic to access the clock manually)
 	//there's not much I can do.
+	//
+	//Should'nt really affect anything, since this iteration of the program runs
+	//on a single thread, and there's really no reason to run it multiple times.
 
 	srand((unsigned int)time(NULL));
 
-	float x;
-	float y;
+	if(argc != 2)
+	{
+		printf("Usage: ep1_buffon number_of_needles\n");
+		return -1;
+	}
 
-	get_random_position(&x, &y, 1000.0f, 1000.0f);
-
-	printf("%f %f\n", x, y);
+	//Executing the experiment with the 1st argument of the program as the number of 
+	//needles.
+	buffon_experiment(atoi(argv[1]));
 
 	return 0;
 }
+
+//Be Excellent to Each Other
+//
+//Made under the influence of my amazing 90s spotify playlist.
+//https://open.spotify.com/playlist/1jAXK6e45VyFc7p5Myutg7?si=SgNQ0yV0SdO4hkLOOA1Awg
